1e111c130b4d55d0a8800e0df4648f8e5f4aa60c
takamano <tamano1118@gmail.com>
2025-06-17 12:34:29 +0900

    Lambda Added.

--- amplify/functions/hello/handler.ts
+++ amplify/functions/hello/handler.ts
@@ -1,8 +1,11 @@
 
 import type { Schema } from "../../data/resource"
+import { env } from "$amplify/env/sayHello";
 type handlerType = Schema['sayHello']['functionHandler']
 
 export const handler: handlerType = async(event, context) => {
+    const region = env.AWS_REGION;
+    const tableName = env.TABLE_NAME
     const { name } = event.arguments
-    return `Hello, ${name}!`
+    return `Hello, ${name}, table name: ${tableName}!`
 }
--- amplify/functions/hello/resource.ts
+++ amplify/functions/hello/resource.ts
@@ -1,9 +1,16 @@
 import { defineFunction } from '@aws-amplify/backend';
 
+export const REGION = "eu-central-1";
+export const TABLE_NAME = "products";
+
 export const sayHello = defineFunction({
     name: 'sayHello',
     entry: './handler.ts',
     bundling: {
         minify: false
+    },
+    environment: {
+        REGION,
+        TABLE_NAME
     }
 })
